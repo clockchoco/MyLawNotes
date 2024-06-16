4794608ad8d2451d4482daba05fee0e7fe25d8d2
ShinEunSeop <clockchoco123@gmail.com>
2024-06-16 18:20:22 +0900

    css adjust2

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>MyLawNotes</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>MyLawNotes</title>
       </Head>
       <body>
         <Main />
